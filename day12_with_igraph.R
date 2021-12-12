de1701d557182d9a9ef64c2b7f4be14a0c6d1284
Olivier Pucher <olivier.pucher@insee.fr>
2021-12-12 12:05:05 +0100

    Update day12_with_igraph.R

--- day12_with_igraph.R
+++ day12_with_igraph.R
@@ -1,21 +1,15 @@
 
 require(igraph)
 
-t <- read_delim("day12test.txt", col_names = FALSE, delim='-')
 day12A <- read_delim("day12A.txt", col_names = FALSE, delim='-')
 
-# A
-
-g <- graph_from_data_frame(t,directed = FALSE)
 g <- graph_from_data_frame(day12A,directed = FALSE)
-
 g %>% plot
-
-maj<-c("HN","LN")
-maj<-c("BN","LP","PK","EP")
 
 c(day12A$X1[substr(day12A$X1,1,1)%in%LETTERS],
   day12A$X2[substr(day12A$X2,1,1)%in%LETTERS])->maj
+
+# A
 
 nbPaths<-function(g,start,end){
   if(!start %in% get.vertex.attribute(g,"name")) return(0)
@@ -26,7 +20,9 @@ nbPaths<-function(g,start,end){
   if(length(nei)==0) return(0)
   if(!start %in% maj){g<-delete_vertices(g,start)}
   ans<-0
-  for(ne in nei){ans<-ans+nbPaths(g,ne,end)}
+  for(ne in nei){
+    ans<-ans+nbPaths(g,ne,end)
+  }
   ans
 }
 
@@ -36,18 +32,13 @@ nbPaths(g,"start","end")
 
 # B
 
-g <- graph_from_data_frame(t,directed = FALSE)
-g <- graph_from_data_frame(day12A,directed = FALSE)
-
-maj<-c("HN","LN")
-maj<-c("BN","LP","PK","EP")
-
 nbPaths2<-function(g,start,end,prev){
   get.vertex.attribute(g,"name")->vertices
   
   if(!start %in% get.vertex.attribute(g,"name")) return(0)
   if(!end %in% get.vertex.attribute(g,"name")) return(0)
   if(start==end) return(1)
+  
   nei<-neighbors(g,start)
   nei<-vertex_attr(g, "name", index = nei)
   
