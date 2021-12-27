702b2aa3b6d99e8c56f508a9321f9a42f44e7f2c
Olivier Pucher <olivier.pucher@insee.fr>
2021-12-27 23:04:35 +0100

    Update day13.R

--- day13.R
+++ day13.R
@@ -6,7 +6,7 @@ instr <- read_delim("day13instr.txt", col_names = FALSE, delim='=')
 # fold along x=655
 
 names(d)<-c("x","y")
-d %>% mutate(xx=x,x=if_else(x>655,655*2-x,x),z=10000*x+y) %>% summarise(sum(!duplicated(z)))
+d %>% mutate(x=if_else(x>655,655*2-x,x),z=10000*x+y) %>% summarise(sum(!duplicated(z)))
 
 # 693
 
@@ -14,9 +14,9 @@ d %>% mutate(xx=x,x=if_else(x>655,655*2-x,x),z=10000*x+y) %>% summarise(sum(!dup
 
 fold<-function(X1,X2){
   if(X1=="fold along x") d<<-d %>% 
-      mutate(xx=x,x=if_else(x>X2,X2*2-x,x),z=10000*x+y) %>% filter(!duplicated(z)) 
+      mutate(x=if_else(x>X2,X2*2-x,x),z=10000*x+y) %>% filter(!duplicated(z)) 
   if(X1=="fold along y") d<<-d %>% 
-      mutate(yy=y,y=if_else(y>X2,X2*2-y,y),z=10000*x+y) %>% filter(!duplicated(z)) 
+      mutate(y=if_else(y>X2,X2*2-y,y),z=10000*x+y) %>% filter(!duplicated(z)) 
 }
 pmap(instr,fold)
 
